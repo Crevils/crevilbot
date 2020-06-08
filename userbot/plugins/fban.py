fd11864c36efb6f1a146b189dc147dc6eb6e718c
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-06-08 17:14:30 +0530

    Create fban.py

---
+++ userbot/plugins/fban.py
@@ -0,0 +1,21 @@
+from telethon import events
+import io
+import asyncio
+from uniborg.util import admin_cmd
+from userbot import FBAN_USER, FBAN_REASON
+
+USER = str(FBAN_USER)       
+REASON = str(FBAN_REASON) if FBAN_REASON else "no reason given"
+
+
+@borg.on(admin_cmd(pattern=r"fban"))
+
+async def _(event):
+    if event.fwd_from:
+        return
+            await borg.send_message("/start")
+            await asyncio.sleep(2)
+            await borg.send_message("/joinfed 70875a92-9b37-467e-abf1-a265d92221")
+            await asyncio.sleep(2)
+            await borg.send_messagef("/fban {USER} {REASON}")
+            await asyncio.sleep(2)
